c154db85d12c980b24eb6645267fef6cb3f1d17f
SatisfiedBucket <117303947+SatisfiedBucket@users.noreply.github.com>
2023-03-08 17:46:37 +0000

    Update Depth.lua

--- Depth.lua
+++ Depth.lua
@@ -25,7 +25,7 @@ local entityTable = Spawner.createEntity({
     Speed = 200, -- Percentage, 100 = default Rush speed
     DelayTime = 0, -- Time before starting cycles (seconds)
     HeightOffset = 0,
-    CanKill = false,
+    CanKill = true,
     KillRange = 50,
     BackwardsMovement = false,
     BreakLights = true,
