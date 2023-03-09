c61221b27124d93cb389150a4bf8c487ef0167e7
SatisfiedBucket <117303947+SatisfiedBucket@users.noreply.github.com>
2023-03-09 19:56:04 +0000

    Update FunnyManualExecute.lua

--- FunnyManualExecute.lua
+++ FunnyManualExecute.lua
@@ -1,1 +1,2 @@
-
+require(game.StarterGui.MainUI.Initiator.Main_Game.RemoteListener.Modules.Screech)(require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game),
+workspace.CurrentRooms[game.Players.LocalPlayer:GetAttribute("CurrentRoom")])
